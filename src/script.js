4af8364a2e063e9c6caa0755e96cf287f7e86372
Career404 <105638831+Career404@users.noreply.github.com>
2022-11-22 16:41:33 +0400

    init: create desktop page

---
+++ src/script.js
@@ -0,0 +1,32 @@
+// eventhandler moves scroll-icon on scrolldown
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  let icon = document.getElementById("scrollIcon")
+  if (prevScrollpos < currentScrollPos) {
+    icon.style.bottom = "2%";
+  }
+  prevScrollpos = currentScrollPos;
+}
+//Intersectoin observer shows burger when user scrolls to .stars
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+  let observer = new IntersectionObserver(entries => {
+    let burger = document.getElementById('burgerMenu')
+    let modalBurger = document.getElementById('modalBurgerMenu')
+  if (entries[0].boundingClientRect.y < 0) {
+    burger.style.display = "block"
+    burger.style.right = "0";
+  } else {
+    burger.style.right = "-500px";
+  }
+  if(document.URL.indexOf("modal") >= 0){
+    burger.style.display = "none"
+
+  }
+});
+observer.observe(document.querySelector(".anchor"));
+}
