459c84d75e9a99105a36e1cb291ee31b4c6f3ed9
Career404 <105638831+Career404@users.noreply.github.com>
2022-11-23 14:32:05 +0400

    feat: improve scrollIcon logic, modal style

--- src/script.js
+++ src/script.js
@@ -3,11 +3,19 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   let icon = document.getElementById("scrollIcon")
+  let endScrollAnchor = document.getElementsByTagName('h3')
+  //console.log(endScrollAnchor)
   if (prevScrollpos < currentScrollPos) {
     icon.style.bottom = "2%";
   }
+  if (window.pageYOffset > 1400) {
+    icon.style.display = "none"
+  }
+  else{
+    icon.style.display = "block"}
   prevScrollpos = currentScrollPos;
 }
+
 //Intersectoin observer shows burger when user scrolls to .stars
 if (
   "IntersectionObserver" in window &&
@@ -16,7 +24,6 @@ if (
 ) {
   let observer = new IntersectionObserver(entries => {
     let burger = document.getElementById('burgerMenu')
-    let modalBurger = document.getElementById('modalBurgerMenu')
   if (entries[0].boundingClientRect.y < 0) {
     burger.style.display = "block"
     burger.style.right = "0";
